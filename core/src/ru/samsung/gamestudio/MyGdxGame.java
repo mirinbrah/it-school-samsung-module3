c4e3e84c43516dc2ec98a917b8018b7745f5bc28
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2026-07-16 23:02:50 +0400

    fix if to while in main physics cycle

--- core/src/ru/samsung/gamestudio/MyGdxGame.java
+++ core/src/ru/samsung/gamestudio/MyGdxGame.java
@@ -76,7 +76,7 @@ public class MyGdxGame extends Game {
         float delta = Gdx.graphics.getDeltaTime();
         accumulator += Math.min(delta, 0.25f);
 
-        if (accumulator >= STEP_TIME) {
+        while (accumulator >= STEP_TIME) {
             accumulator -= STEP_TIME;
             world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
         }
